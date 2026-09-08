74aefb749d19c1854554049369ce60fd6e8efbbb
LambdaTeam710 <osama-saidy@hotmail.com>
2026-09-08 04:08:50 +0100

    Add : ft_memset.c ft_bezero.c ft_memcpy.c && Update :libft/Makefile

--- libft.h
+++ libft.h
@@ -25,5 +25,8 @@ int	ft_strncmp(const char	*s1, const char	*s2, size_t	n);
 size_t	ft_strlcpy(char	*dst, const char	*src, size_t	dstsize);
 size_t	ft_strlcat(char	*dst, const char	*src, size_t	dstsize);
 char	*ft_strnstr(const char	*big, const char	*little, size_t	len);
+void	*ft_memset(void	*s, int	c, size_t	n);
+void	ft_bzero(void	*s, size_t	n);
+void	*ft_memcpy(void	*dest, const void	*src, size_t	n);
 
 #endif
