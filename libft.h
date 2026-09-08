c300ea53d519e50aff32a7f1cab7e6dbce51c010
LambdaTeam710 <osama-saidy@hotmail.com>
2026-09-08 02:06:41 +0100

    Add : ft_strnstr.c && Update : Makefile/Libft.h

--- libft.h
+++ libft.h
@@ -24,5 +24,6 @@ char	*ft_trrchr(const char	*s, int	c);
 int	ft_strncmp(const char	*s1, const char	*s2, size_t	n);
 size_t	ft_strlcpy(char	*dst, const char	*src, size_t	dstsize);
 size_t	ft_strlcat(char	*dst, const char	*src, size_t	dstsize);
+char	*ft_strnstr(const char	*big, const char	*little, size_t	len);
 
 #endif
