da292649bf03f56ec5d011fecfd35fa64f3f80ce
LambdaTeam710 <osama-saidy@hotmail.com>
2026-09-12 07:14:14 +0100

    Add : ft_putchar_fd.c ft_putstr_fd.c ft_putendl_fd.c ft_putnbr_fd.c && Update : Makefile/libft.h

--- libft.h
+++ libft.h
@@ -3,6 +3,7 @@
 
 #include <stddef.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 
 typedef struct s_list
@@ -34,6 +35,10 @@ void	*ft_memchr(const void	*s, int	c, size_t	n);
 int	ft_memcmp(const void	*s1, const void	*s2, size_t	n);
 int	ft_atoi(const char	*nptr);
 void	*ft_calloc(size_t	nmemb, size_t	size);
-char	*ft_strdup.c(const char	*s);
+char	*ft_strdup(const char	*s);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char	*s, int	fd);
+void	ft_putendl_fd(char	*s, int	fd);
+void	ft_putnbr_fd(int	n, int	fd);
 
 #endif
