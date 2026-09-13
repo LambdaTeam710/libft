72639e18e83c9ca3200bbabe72b10ba622c72cd7
LambdaTeam710 <osama-saidy@hotmail.com>
2026-09-13 06:25:30 +0100

    Add : ft_substr.c && Update : Makefile/libft.h

---
+++ ft_substr.c
@@ -0,0 +1,28 @@
+#include "libft.h"
+
+char	*ft_substr(char const	*s, unsigned int	start, size_t	len)
+{
+	char	*sub_str;
+	size_t	s_len;
+
+	if (!s)
+	{
+		return (NULL);
+	}
+	s_len = ft_strlen(s);
+	if (start >= s_len)
+	{
+		return (ft_strdup(""));
+	}
+	if (len > s_len - start)
+	{
+		len = s_len - start;
+	}
+	sub_str = malloc(len + 1);
+	if (!sub_str)
+	{
+		return (NULL);
+	}
+	ft_strlcpy(sub_str, (s + start), (len + 1));
+	return (sub_str);
+}
