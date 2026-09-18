9f1fa03a0f4f6836df8a76a51da3e47a018b2aec
LambdaTeam710 <osama-saidy@hotmail.com>
2026-09-18 12:16:03 +0100

    Final Core logic Reimplemetation of ft_split.c && Modify : Makefile/libft.h

--- ft_split.c
+++ ft_split.c
@@ -31,13 +31,56 @@ static int	word_len(const char	*s, char	c)
 	return (len);
 }
 
-static char	*word_make(const char	*s, int	start, int	len)
+static void	split_free(char	**array, int	created_count)
 {
-	char	*new_word;
-
-	new_word = ft_substr(s, start, len);
-	return (new_word);
+	while (created_count > 0)
+	{
+		free(array[created_count - 1]);
+		created_count--;
+	}
+	free(array);
 }
-//char	**ft_split(const char	*s, char	c);
-//{
-//}
+
+static char	**array_fill(char	**array, const char *s, char c)
+{
+	int	i;
+	int	wi;
+	int	length;
+	char	*new;
+
+	i = 0;
+	wi = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			i++;
+		else
+		{
+			length = word_len(s + i, c);
+			new = ft_substr(s, i, length);
+			if (!new)
+			{
+				split_free(array, wi);
+				return (NULL);
+			}
+			array[wi] = new;
+			wi++;
+			i = i + length;
+		}
+	}
+	array[wi] = NULL;
+	return (array);
+}
+char	**ft_split(const char	*s, char	c)
+{
+	char **array;
+	int	counter;
+
+	if (!s)
+		return (NULL);
+	counter = word_count(s, c);
+	array = malloc((counter + 1) * sizeof(char *));
+	if (!array)
+		return (NULL);
+	return (array_fill(array, s, c));
+}
--- libft.h
+++ libft.h
@@ -45,5 +45,6 @@ char	*ft_strjoin(char const	*s1, char const	*s2);
 char	*ft_strtrim(char const	*s1, char const	*set);
 char	*ft_strmapi(char const	*s, char	(*f)(unsigned int, char));
 void	ft_striteri(char	*s, void	(*f)(unsigned int, char *));
+char	**ft_split(const char	*s, char	c);
 
 #endif
