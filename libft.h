aee2a3ced4635c428f8166e37d3885eb831f4bc9
LambdaTeam710 <osama-saidy@hotmail.com>
2026-09-20 08:49:12 +0100

    Add : ft_lstnew.c ft_lstadd_front.c ft_lstsize.c && Update : Makefile/libft.h

--- libft.h
+++ libft.h
@@ -47,5 +47,8 @@ char	*ft_strmapi(char const	*s, char	(*f)(unsigned int, char));
 void	ft_striteri(char	*s, void	(*f)(unsigned int, char *));
 char	**ft_split(const char	*s, char	c);
 char	*ft_itoa(int	n);
+t_list	*ft_lstnew(void	*content);
+void	ft_lstadd_front(t_list	**lst, t_list	*new);
+int	ft_lstsize(t_list	*lst);
 
 #endif
